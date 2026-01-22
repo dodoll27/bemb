6634c2204482e195216c407dd8bb042058161457
Teodora <83964749+dodoll27@users.noreply.github.com>
2026-01-22 20:14:57 +0100

    add requests

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -17,4 +17,27 @@ class RecipeController extends Controller
         $recipe = Recipe::create($request->all());
         return response()->json($recipe, 201);
     }
+
+    public function update(Request $request, string $id)
+    {
+        $recipe = Recipe::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|string|max:255',
+            'instructions' => 'sometimes|string',
+            'nutritionalValue' => 'sometimes|array',
+            'isPrivate' => 'sometimes|boolean',
+        ]);
+
+        $recipe->update($validated);
+
+        return response()->json($recipe);
+    }
+
+    public function destroy(string $id)
+    {
+        $recipe = Recipe::findOrFail($id);
+        $recipe->delete();
+        return response()->json(null, 204);
+    }
 }
--- routes/user.php
+++ routes/user.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\UserController;
 
 Route::post('/user', [UserController::class, 'store']);
 Route::get('/user/{id}', [UserController::class, 'show']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
+Route::put('/user/{id}', [UserController::class, 'update']);
